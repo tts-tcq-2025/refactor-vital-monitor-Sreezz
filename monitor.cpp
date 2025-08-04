22e3302b7e95406b1fca71da25c58eb0e2546076
Sreezz <65155642+Sreezz@users.noreply.github.com>
2025-08-04 22:32:39 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -5,34 +5,88 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
+
+enum VitalStatus 
+{
+    OK,
+    TemperatureCritical,
+    PulseOutOfRange,
+    Spo2Low
+};
+
+
+bool isTemperatureCritical(float temperature) 
+{
+    return (temperature > 102 || temperature < 95);
 }
+
+bool isPulseOutOfRange(float pulseRate) 
+{
+    return (pulseRate < 60 || pulseRate > 100);
+}
+
+bool isSpo2Low(float spo2) 
+{
+    return (spo2 < 90);
+}
+
+enum VitalStatus evaluateVitals(float temperature, float pulseRate, float spo2) 
+{
+    if (isTemperatureCritical(temperature)) 
+    {
+        return TemperatureCritical;
+    }
+    if (isPulseOutOfRange(pulseRate)) 
+    {
+        return PulseOutOfRange;
+    }
+    if (isSpo2Low(spo2)) 
+    {
+        return Spo2Low;
+    }
+    return OK;
+}
+
+void showAlert(enum VitalStatus status) 
+{
+      switch (status) 
+    {
+        case TemperatureCritical:
+            cout << "Temperature is critical!\n";
+            break;
+        case PulseOutOfRange:
+            cout << "Pulse Rate is out of range!\n";
+            break;
+        case Spo2Low:
+            cout << "Oxygen Saturation out of range!\n";
+            break;
+        default:
+            //
+            break;
+    }
+    for (int i = 0; i < 6; i++) 
+    {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
+}
+
+
+int vitalsOk(float temperature, float pulseRate, float spo2) 
+{
+    enum VitalStatus status = evaluateVitals(temperature, pulseRate, spo2);
+    showAlert(status);
+
+    if(status == OK)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  
+}
+
