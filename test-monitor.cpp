e9fb990f35c1954f0db3c2ced4b14cf069c06af8
Sreezz <65155642+Sreezz@users.noreply.github.com>
2025-08-18 23:00:57 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,16 +1,16 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) 
-{
+TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 
-  ASSERT_FALSE(vitalsOk(103, 70, 95)); // Temperature > 102
-  ASSERT_FALSE(vitalsOk(94.9, 70, 95)); // Temperature < 95
-  ASSERT_FALSE(vitalsOk(98.6, 101, 95)); // Pulse > 100
-  ASSERT_FALSE(vitalsOk(98.6, 59, 95)); // Pulse < 60
-  ASSERT_FALSE(vitalsOk(98.6, 70, 89.9)); // Spo2 < 90
-  ASSERT_FALSE(vitalsOk(103, 101, 89)); // All vitals not ok
+  ASSERT_FALSE(vitalsOk(103, 70, 95));  // Temperature > 102
+  ASSERT_FALSE(vitalsOk(94.9, 70, 95));  // Temperature < 95
+  ASSERT_FALSE(vitalsOk(98.6, 101, 95));  // Pulse > 100
+  ASSERT_FALSE(vitalsOk(98.6, 59, 95));  // Pulse < 60
+  ASSERT_FALSE(vitalsOk(98.6, 70, 89.9));  // Spo2 < 90
+  ASSERT_FALSE(vitalsOk(103, 101, 89));  // All vitals not ok
 }
+
 
