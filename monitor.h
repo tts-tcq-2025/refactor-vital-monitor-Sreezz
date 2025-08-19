af0090059097f49722f1105b108ddd61a686fa3a
Sreezz <65155642+Sreezz@users.noreply.github.com>
2025-08-19 11:02:09 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,22 @@
 #pragma once
 
+enum Vital {
+    VitalTemperature,
+    VitalPulse,
+    VitalSpo2,
+    VitalCount
+};
+
+enum Language {
+    LANG_ENGLISH,
+    LANG_GERMAN,
+    LANG_COUNT
+};
+
+typedef struct {
+    enum Vital status;
+    const char* alertMessages[LANG_COUNT];
+} VitalInfo;
+
 int vitalsOk(float temperature, float pulseRate, float spo2);
+
